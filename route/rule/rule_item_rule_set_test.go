cf3f083763f5b876d686f702148f83f9ff521c97
PuerNya <safarier@outlook.com>
2024-08-13 06:42:47 +0800

    add rule-provider clash-api

--- route/rule/rule_item_rule_set_test.go
+++ route/rule/rule_item_rule_set_test.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/sagernet/sing-box/adapter"
 	N "github.com/sagernet/sing/common/network"
@@ -52,6 +53,13 @@ func (r *ruleSetItemTestRouter) NeighborResolver() adapter.NeighborResolver { re
 func (r *ruleSetItemTestRouter) AppendTracker(adapter.ConnectionTracker)    {}
 func (r *ruleSetItemTestRouter) ResetNetwork()                              {}
 func (r *ruleSetItemTestRouter) Reload() {}
+func (r *ruleSetItemTestRouter) RuleSets() []adapter.RuleSet {
+	var result []adapter.RuleSet
+	for _, rs := range r.ruleSets {
+		result = append(result, rs)
+	}
+	return result
+}
 
 type countingRuleSet struct {
 	name string
@@ -59,6 +67,14 @@ type countingRuleSet struct {
 }
 
 func (s *countingRuleSet) Name() string { return s.name }
+
+func (s *countingRuleSet) Type() string { return "local" }
+
+func (s *countingRuleSet) Format() string { return "source" }
+
+func (s *countingRuleSet) UpdatedTime() time.Time { return time.Time{} }
+
+func (s *countingRuleSet) Update(context.Context) error { return nil }
 
 func (s *countingRuleSet) StartContext(context.Context, *adapter.HTTPStartContext) error { return nil }
 
@@ -81,6 +97,8 @@ func (s *countingRuleSet) RegisterCallback(adapter.RuleSetUpdateCallback) *list.
 }
 func (s *countingRuleSet) UnregisterCallback(*list.Element[adapter.RuleSetUpdateCallback]) {}
 func (s *countingRuleSet) Close() error                                                    { return nil }
+
+func (s *countingRuleSet) RuleCount() uint64 { return 0 }
 
 func (s *countingRuleSet) Match(*adapter.InboundContext) bool { return true }
 
