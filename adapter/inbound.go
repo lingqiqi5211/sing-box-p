b9daa248f630662b4a7f4750da7f0196beb812f0
reF1nd <ref1nd@thunder-cloud.cc>
2025-03-19 15:21:36 +0800

    Add `match_only` option for resolve action

--- adapter/inbound.go
+++ adapter/inbound.go
@@ -70,6 +70,8 @@ type InboundContext struct {
 
 	// cache
 
+	CacheIPs []netip.Addr
+
 	// Deprecated: implement in rule action
 	InboundDetour             string
 	LastInbound               string
