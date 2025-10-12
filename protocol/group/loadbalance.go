b4ceedd07a8cf765c2760e9af595fd26024101da
铃柒柒 <lingqiqi233@gmail.com>
2025-10-12 14:31:42 +0800

    remove SniffHost

--- protocol/group/loadbalance.go
+++ protocol/group/loadbalance.go
@@ -555,8 +555,6 @@ func getKey(metadata *adapter.InboundContext) string {
 	var metadataHost string
 	if metadata.Destination.IsDomain() {
 		metadataHost = metadata.Destination.Fqdn
-	} else if metadata.SniffHost != "" {
-		metadataHost = metadata.SniffHost
 	} else {
 		metadataHost = metadata.Domain
 	}
