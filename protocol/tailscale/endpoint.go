1a680ac558c00dab7de42f389719b3e35cf6e84c
reF1nd <ref1nd@thunder-cloud.cc>
2026-03-12 18:16:19 +0800

    Add `inner_domain_resolver` for wireguard, tailscale, and socks4 outbounds

--- protocol/tailscale/endpoint.go
+++ protocol/tailscale/endpoint.go
@@ -112,6 +112,8 @@ type Endpoint struct {
 	systemTun           tun.Tun
 	systemDialer        *dialer.DefaultDialer
 	fallbackTCPCloser   func()
+
+	innerDNSQueryOptions adapter.DNSQueryOptions
 }
 
 func (t *Endpoint) registerNetstackHandlers() {
@@ -244,7 +246,7 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		DNS:        &dnsConfigurtor{},
 		HTTPClient: controlHTTPClient,
 	}
-	return &Endpoint{
+	ep := &Endpoint{
 		Adapter:                    endpoint.NewAdapter(C.TypeTailscale, tag, []string{N.NetworkTCP, N.NetworkUDP, N.NetworkICMP}, nil),
 		ctx:                        ctx,
 		router:                     router,
@@ -265,7 +267,15 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		systemInterface:            options.SystemInterface,
 		systemInterfaceName:        options.SystemInterfaceName,
 		systemInterfaceMTU:         options.SystemInterfaceMTU,
-	}, nil
+	}
+	if options.InnerDomainResolver != nil {
+		innerDNSOpts, err := adapter.DNSQueryOptionsFrom(ctx, options.InnerDomainResolver)
+		if err != nil {
+			return nil, E.Cause(err, "inner domain resolver")
+		}
+		ep.innerDNSQueryOptions = *innerDNSOpts
+	}
+	return ep, nil
 }
 
 func (t *Endpoint) Start(stage adapter.StartStage) error {
@@ -511,7 +521,7 @@ func (t *Endpoint) DialContext(ctx context.Context, network string, destination 
 		t.logger.InfoContext(ctx, "outbound packet connection to ", destination)
 	}
 	if destination.IsDomain() {
-		destinationAddresses, err := t.dnsRouter.Lookup(ctx, destination.Fqdn, adapter.DNSQueryOptions{})
+		destinationAddresses, err := t.dnsRouter.Lookup(ctx, destination.Fqdn, t.innerDNSQueryOptions)
 		if err != nil {
 			return nil, err
 		}
@@ -597,7 +607,7 @@ func (t *Endpoint) listenPacketWithAddress(ctx context.Context, destination M.So
 func (t *Endpoint) ListenPacketWithDestination(ctx context.Context, destination M.Socksaddr) (net.PacketConn, netip.Addr, error) {
 	t.logger.InfoContext(ctx, "outbound packet connection to ", destination)
 	if destination.IsDomain() {
-		destinationAddresses, err := t.dnsRouter.Lookup(ctx, destination.Fqdn, adapter.DNSQueryOptions{})
+		destinationAddresses, err := t.dnsRouter.Lookup(ctx, destination.Fqdn, t.innerDNSQueryOptions)
 		if err != nil {
 			return nil, netip.Addr{}, err
 		}
