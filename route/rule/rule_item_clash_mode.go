sjjcyn/sing-box

support multi `clash_mode` item in rule item

route/rule_item_clash_mode.go
@@ -4,19 +4,20 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing/common"
 )
 
 var _ RuleItem = (*ClashModeItem)(nil)
 
 type ClashModeItem struct {
 	router adapter.Router
-	mode   string
+	modes  []string
 }
 
-func NewClashModeItem(router adapter.Router, mode string) *ClashModeItem {
+func NewClashModeItem(router adapter.Router, modes []string) *ClashModeItem {
 	return &ClashModeItem{
 		router: router,
-		mode:   mode,
+		modes:  modes,
 	}
 }
 
@@ -25,9 +26,15 @@ func (r *ClashModeItem) Match(metadata *adapter.InboundContext) bool {
 	if clashServer == nil {
 		return false
 	}
-	return strings.EqualFold(clashServer.Mode(), r.mode)
+	return common.Any(r.modes, func(mode string) bool {
+		return strings.EqualFold(clashServer.Mode(), mode)
+	})
 }
 
 func (r *ClashModeItem) String() string {
-	return "clash_mode=" + r.mode
+	modeStr := r.modes[0]
+	if len(r.modes) > 1 {
+		modeStr = "[" + strings.Join(r.modes, ", ") + "]"
+	}
+	return "clash_mode=" + modeStr
 }

lingqiqi5211/sing-box-p

route/rule/rule_item_clash_mode.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/service"
 )
 
@@ -13,13 +14,13 @@ var _ RuleItem = (*ClashModeItem)(nil)
 type ClashModeItem struct {
 	ctx         context.Context
 	clashServer adapter.ClashServer
-	mode        string
+	modes       []string
 }
 
-func NewClashModeItem(ctx context.Context, mode string) *ClashModeItem {
+func NewClashModeItem(ctx context.Context, modes []string) *ClashModeItem {
 	return &ClashModeItem{
-		ctx:  ctx,
-		mode: mode,
+		ctx:   ctx,
+		modes: modes,
 	}
 }
 
@@ -32,9 +33,15 @@ func (r *ClashModeItem) Match(metadata *adapter.InboundContext) bool {
 	if r.clashServer == nil {
 		return false
 	}
-	return strings.EqualFold(r.clashServer.Mode(), r.mode)
+	return common.Any(r.modes, func(mode string) bool {
+		return strings.EqualFold(r.clashServer.Mode(), mode)
+	})
 }
 
 func (r *ClashModeItem) String() string {
-	return "clash_mode=" + r.mode
+	modeStr := r.modes[0]
+	if len(r.modes) > 1 {
+		modeStr = "[" + strings.Join(r.modes, ", ") + "]"
+	}
+	return "clash_mode=" + modeStr
 }