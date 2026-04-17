reF1nd/sing-box

Add `domain_match_strategy` and `default_domain_match_strategy`

route/rule/rule_item_domain.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing/common/domain"
 	E "github.com/sagernet/sing/common/exceptions"
 )
@@ -11,11 +12,12 @@ import (
 var _ RuleItem = (*DomainItem)(nil)
 
 type DomainItem struct {
-	matcher     *domain.Matcher
-	description string
+	matcher             *domain.Matcher
+	description         string
+	domainMatchStrategy C.DomainMatchStrategy
 }
 
-func NewDomainItem(domains []string, domainSuffixes []string) (*DomainItem, error) {
+func NewDomainItem(domains []string, domainSuffixes []string, domainMatchStrategy C.DomainMatchStrategy) (*DomainItem, error) {
 	for _, domainItem := range domains {
 		if domainItem == "" {
 			return nil, E.New("domain: empty item is not allowed")
@@ -51,24 +53,45 @@ func NewDomainItem(domains []string, domainSuffixes []string) (*DomainItem, erro
 	return &DomainItem{
 		domain.NewMatcher(domains, domainSuffixes, false),
 		description,
+		domainMatchStrategy,
 	}, nil
 }
 
-func NewRawDomainItem(matcher *domain.Matcher) *DomainItem {
+func NewRawDomainItem(matcher *domain.Matcher, domainMatchStrategy C.DomainMatchStrategy) *DomainItem {
 	return &DomainItem{
 		matcher,
 		"domain/domain_suffix=<binary>",
+		domainMatchStrategy,
 	}
 }
 
 func (r *DomainItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.Destination.IsFqdn() {
-		domainHost = metadata.Destination.Fqdn
-	} else if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else {
-		domainHost = metadata.Domain
+	switch r.domainMatchStrategy {
+	case C.DomainMatchStrategyPreferSniffHost:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		} else {
+			domainHost = metadata.Domain
+		}
+	case C.DomainMatchStrategyFQDNOnly:
+		if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		}
+	case C.DomainMatchStrategySniffHostOnly:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		}
+	default:
+		if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		} else if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else {
+			domainHost = metadata.Domain
+		}
 	}
 	if domainHost == "" {
 		return false

route/rule/rule_item_domain_regex.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	C "github.com/sagernet/sing-box/constant"
 	E "github.com/sagernet/sing/common/exceptions"
 	F "github.com/sagernet/sing/common/format"
 )
@@ -12,11 +13,12 @@ import (
 var _ RuleItem = (*DomainRegexItem)(nil)
 
 type DomainRegexItem struct {
-	matchers    []*regexp.Regexp
-	description string
+	matchers            []*regexp.Regexp
+	description         string
+	domainMatchStrategy C.DomainMatchStrategy
 }
 
-func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
+func NewDomainRegexItem(expressions []string, domainMatchStrategy C.DomainMatchStrategy) (*DomainRegexItem, error) {
 	matchers := make([]*regexp.Regexp, 0, len(expressions))
 	for i, regex := range expressions {
 		matcher, err := regexp.Compile(regex)
@@ -34,17 +36,36 @@ func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
 	} else {
 		description += F.ToString("[", strings.Join(expressions, " "), "]")
 	}
-	return &DomainRegexItem{matchers, description}, nil
+	return &DomainRegexItem{matchers, description, domainMatchStrategy}, nil
 }
 
 func (r *DomainRegexItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.Destination.IsFqdn() {
-		domainHost = metadata.Destination.Fqdn
-	} else if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else {
-		domainHost = metadata.Domain
+	switch r.domainMatchStrategy {
+	case C.DomainMatchStrategyPreferSniffHost:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		} else {
+			domainHost = metadata.Domain
+		}
+	case C.DomainMatchStrategyFQDNOnly:
+		if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		}
+	case C.DomainMatchStrategySniffHostOnly:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		}
+	default:
+		if metadata.Destination.IsFqdn() {
+			domainHost = metadata.Destination.Fqdn
+		} else if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else {
+			domainHost = metadata.Domain
+		}
 	}
 	if domainHost == "" {
 		return false

lingqiqi5211/sing-box-p

route/rule/rule_item_domain.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing/common/domain"
 	E "github.com/sagernet/sing/common/exceptions"
 )
@@ -11,11 +12,12 @@ import (
 var _ RuleItem = (*DomainItem)(nil)
 
 type DomainItem struct {
-	matcher     *domain.Matcher
-	description string
+	matcher             *domain.Matcher
+	description         string
+	domainMatchStrategy C.DomainMatchStrategy
 }
 
-func NewDomainItem(domains []string, domainSuffixes []string) (*DomainItem, error) {
+func NewDomainItem(domains []string, domainSuffixes []string, domainMatchStrategy C.DomainMatchStrategy) (*DomainItem, error) {
 	for _, domainItem := range domains {
 		if domainItem == "" {
 			return nil, E.New("domain: empty item is not allowed")
@@ -51,24 +53,45 @@ func NewDomainItem(domains []string, domainSuffixes []string) (*DomainItem, erro
 	return &DomainItem{
 		domain.NewMatcher(domains, domainSuffixes, false),
 		description,
+		domainMatchStrategy,
 	}, nil
 }
 
-func NewRawDomainItem(matcher *domain.Matcher) *DomainItem {
+func NewRawDomainItem(matcher *domain.Matcher, domainMatchStrategy C.DomainMatchStrategy) *DomainItem {
 	return &DomainItem{
 		matcher,
 		"domain/domain_suffix=<binary>",
+		domainMatchStrategy,
 	}
 }
 
 func (r *DomainItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Destination.IsDomain() {
-		domainHost = metadata.Destination.Fqdn
-	} else {
-		domainHost = metadata.Domain
+	switch r.domainMatchStrategy {
+	case C.DomainMatchStrategyPreferFQDN:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else {
+			domainHost = metadata.Domain
+		}
+	case C.DomainMatchStrategyFQDNOnly:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		}
+	case C.DomainMatchStrategySniffHostOnly:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		}
+	default:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else {
+			domainHost = metadata.Domain
+		}
 	}
 	if domainHost == "" {
 		return false

route/rule/rule_item_domain_regex.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	C "github.com/sagernet/sing-box/constant"
 	E "github.com/sagernet/sing/common/exceptions"
 	F "github.com/sagernet/sing/common/format"
 )
@@ -12,11 +13,12 @@ import (
 var _ RuleItem = (*DomainRegexItem)(nil)
 
 type DomainRegexItem struct {
-	matchers    []*regexp.Regexp
-	description string
+	matchers            []*regexp.Regexp
+	description         string
+	domainMatchStrategy C.DomainMatchStrategy
 }
 
-func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
+func NewDomainRegexItem(expressions []string, domainMatchStrategy C.DomainMatchStrategy) (*DomainRegexItem, error) {
 	matchers := make([]*regexp.Regexp, 0, len(expressions))
 	for i, regex := range expressions {
 		matcher, err := regexp.Compile(regex)
@@ -34,17 +36,36 @@ func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
 	} else {
 		description += F.ToString("[", strings.Join(expressions, " "), "]")
 	}
-	return &DomainRegexItem{matchers, description}, nil
+	return &DomainRegexItem{matchers, description, domainMatchStrategy}, nil
 }
 
 func (r *DomainRegexItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Destination.IsDomain() {
-		domainHost = metadata.Destination.Fqdn
-	} else {
-		domainHost = metadata.Domain
+	switch r.domainMatchStrategy {
+	case C.DomainMatchStrategyPreferFQDN:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else {
+			domainHost = metadata.Domain
+		}
+	case C.DomainMatchStrategyFQDNOnly:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		}
+	case C.DomainMatchStrategySniffHostOnly:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		}
+	default:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else {
+			domainHost = metadata.Domain
+		}
 	}
 	if domainHost == "" {
 		return false