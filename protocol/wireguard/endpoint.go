8bdf0fa5f0e62d5bcf3f7442d0b128b7262462ad
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-19 00:53:06 +0800

    Resolve L3 domains via selected outbound

--- protocol/wireguard/endpoint.go
+++ protocol/wireguard/endpoint.go
@@ -28,6 +28,7 @@ import (
 
 var (
 	_ adapter.OutboundWithPreferredRoutes = (*Endpoint)(nil)
+	_ adapter.FlowOutboundDomainResolver  = (*Endpoint)(nil)
 	_ adapter.InterfaceUpdateListener     = (*Endpoint)(nil)
 	_ dialer.PacketDialerWithDestination  = (*Endpoint)(nil)
 )
@@ -184,6 +185,10 @@ func (w *Endpoint) updateBind(ctx context.Context) {
 
 func (w *Endpoint) PreMatchFlow(network string, destination netip.Addr) adapter.PreMatchAction {
 	return adapter.PreMatchFlow
+}
+
+func (w *Endpoint) FlowDomainResolveOptions() adapter.DNSQueryOptions {
+	return w.innerDNSQueryOptions
 }
 
 func (w *Endpoint) PortAddresses() (netip.Addr, netip.Addr) {
